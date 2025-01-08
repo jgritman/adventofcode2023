0a8cf58437011dccd84e96e2d3bebdd458310da7
Jason Gritman <jasongritman@gmail.com>
2025-01-08 15:49:31 -0700

    day 1 part 1 - calibration values

---
+++ day01/Part1.kts
@@ -0,0 +1,17 @@
+import java.io.File
+
+val fileName = "input.txt"
+
+fun processLine(line: String): Int {
+    val first = line.first { it.isDigit() }
+    val last = line.last { it.isDigit() }
+    return "$first$last".toInt()
+}
+
+// Use lazy sequence to compute the sum
+val sum = File(fileName).useLines { lines ->
+    lines.map { line -> processLine(line) }.sum()
+}
+
+println("Total sum: $sum")
+
